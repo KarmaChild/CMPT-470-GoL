e88c9b96c7c8564db90e246856b6b50f75a3baae
adith <adm115@mail.usask.ca>
2022-09-27 10:48:28 -0600

    build: add crate array2d = "0.2.1"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#[allow(unused_variables)]
+use array2d::Array2D;
+use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+
+    fn make_grid(rows:usize,cols:usize) -> Array2D<i32> {
+        let mut grid = Array2D::filled_with(0,rows,cols);
+        return grid;
+    }
 }
