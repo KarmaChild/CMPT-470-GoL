c35b0c5340a643111deca6d562ecd379a0a7b9cf
adith <adm115@mail.usask.ca>
2022-09-28 19:33:56 -0600

    fix: fix main()

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+use array2d::Array2D;
+
 mod game_of_life;
 
 fn main() {
-    let grid_a = game_of_life::initialize_grid(game_of_life::read_file_to_string_buffer("input.txt"),game_of_life::make_grid(100,100));
-    let grid_b = game_of_life::make_grid(100,100);
+
+    let row: usize = 100;
+    let col: usize = 100;
+
+    let grid_a: Array2D<i32> = game_of_life::initialize_grid(game_of_life::read_file_to_string_buffer("input.txt"),game_of_life::make_grid(row,col));
+    let grid_b: Array2D<i32>  = game_of_life::make_grid(row,col);
+
     game_of_life::write_to_file(game_of_life::game_of_life(grid_a,grid_b),"output.txt");
 }
